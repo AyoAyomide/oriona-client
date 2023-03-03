586e8bbef7a5230e7d0b55ca73b2a98143370eef
ayoayomide <ayoayomide124@gmail.com>
2023-03-03 18:50:08 +0100

    test device movement

--- src/js/components/camera.js
+++ src/js/components/camera.js
@@ -1,29 +1,11 @@
 import { gui } from "..";
 
 AFRAME.registerComponent('animation-camera', {
-    schema: {
-
-    },
-
     init: function () {
-        let { components } = this.el;
-        this.rotation = components.rotation.data;
-        this.position = components.position.data;
-        gui.add(this.rotation, 'x').min(-360).max(0).step(-0.001);
-        gui.add(this.rotation, 'y').min(-360).max(0).step(-0.001);
-        gui.add(this.rotation, 'z').min(-360).max(0).step(-0.001);
-
-        gui.add(this.position, 'x').min(0).max(100).step(0.001);
-        gui.add(this.position, 'y').min(0).max(100).step(0.001);
-        gui.add(this.position, 'z').min(-5).max(0).step(-0.001);
-
-
-        // this.el.setAttribute('position', pos);
-        // this.el.setAttribute('rotation', pos);
-        console.log(components.position.data, components.rotation.data);
     },
 
     update: function () {
+        this.el.object3D.rotation.y = this.rotation.y;
         // Do something when component's data is updated.
     },
 
@@ -32,8 +14,6 @@ AFRAME.registerComponent('animation-camera', {
     },
 
     tick: function (time, timeDelta) {
-        // this.el.setAttribute('rotation', this.rotation);
-        // this.el.setAttribute('position', this.position);
-        // Do something on every scene tick or frame.
+    
     }
 });
---
+++ src/js/components/move.js
@@ -0,0 +1,28 @@
+AFRAME.registerComponent('move', {
+    init: function () {
+        // Do something when component first attached.
+        let camera = document.querySelector('a-camera');
+        this.el.addEventListener('thumbstickmoved', (events) => (this.logThumbstick(events, camera)));
+        console.log(camera.object3D);
+    },
+    logThumbstick: function (evt, object) {
+        if (evt.detail.y > 0.95) { console.log("DOWN"); moveObject(object, 'BACKWARD') }
+        if (evt.detail.y < -0.95) { console.log("UP"); moveObject(object, 'FORWARD') }
+        if (evt.detail.x < -0.95) { console.log("LEFT"); moveObject(object, 'LEFT') }
+        if (evt.detail.x > 0.95) { console.log("RIGHT"); moveObject(object, 'RIGHT') }
+    }
+});
+
+function moveObject(object, direction) {
+    let speed = 0.1,
+        calc = {
+            BACKWARD: () => { object.object3D.position.z += speed; },
+            FORWARD: () => { object.object3D.position.z -= speed },
+            LEFT: () => { object.object3D.position.x -= speed },
+            RIGHT: () => { object.object3D.position.x += speed },
+            UP: () => { object.object3D.position.y += speed },
+            DOWN: () => { object.object3D.position.y -= speed }
+        };
+    calc[direction]();
+}
+
