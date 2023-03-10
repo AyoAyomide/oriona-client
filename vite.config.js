8141800c7c301fcf674c467dfbe06ebfa9506d74
ayoayomide <ayoayomide124@gmail.com>
2023-03-10 11:10:31 +0100

    glb import support

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,25 @@
 import { defineConfig } from "vite";
+import { viteStaticCopy } from 'vite-plugin-static-copy'
 
-export default defineConfig((command, mode)=>{
+export default defineConfig((command, mode) => {
   console.log(command);
   if (command === 'serve') {
     return {
     }
   } else {
     return {
-      base:'/vr-animation/'
+      base: '/vr-animation/',
+      assetsInclude: ['**/*.glb'],
+      plugins: [
+        viteStaticCopy({
+          targets: [
+            {
+              src: 'src/gltf/*.glb',
+              dest: 'src/gltf'
+            }
+          ]
+        })
+      ]
     }
   }
 })
